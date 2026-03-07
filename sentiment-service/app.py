f0321977cdc69bb0bf4edc53bc330d50554b10b5
gabrielWYP <gawayspa@gmail.com>
2026-03-07 09:36:39 -0500

    try ci/cd

--- sentiment-service/app.py
+++ sentiment-service/app.py
@@ -11,8 +11,6 @@ from pydantic import BaseModel
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-#loggin ci/cd
-
 # ==========================================
 # CONFIGURACIÓN DE ENTORNO
 # ==========================================
